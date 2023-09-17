b7c0fbf567d86cf27b8659bbb3c71d20313aaec4
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2023-09-17 23:34:40 +0900

    Create main.py

---
+++ style_transfer/main.py
@@ -0,0 +1,17 @@
+import os
+import tensorflow as tf
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+import IPython.display as display
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+import PIL.Image
+import time
+import functools
+
