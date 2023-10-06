83dda35495ba66e0a89f9017af4019849e2d2678
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2023-10-07 01:23:03 +0900

    create diffusion model

--- diffusion_model/main.py
+++ diffusion_model/main.py
@@ -1,8 +1,3 @@
-
-"""
-For more detail:
-https://tree.rocks/make-diffusion-model-from-scratch-easy-way-to-implement-quick-diffusion-model-e60d18fd0f2e
-"""
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
@@ -10,182 +5,43 @@ from keras import layers
 import numpy as np
 from tqdm.auto import trange, tqdm
 import matplotlib.pyplot as plt
-
+import func
 
 for gpu in tf.config.list_physical_devices('GPU'):
     tf.config.experimental.set_memory_growth(gpu, True)
-
-
+    
 # load dataset
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
 X_train = X_train[y_train.squeeze() == 1]
 X_train = (X_train / 127.5) - 1.0
+
+# define var
 IMG_SIZE = 32     # input image size, CIFAR-10 is 32x32
-BATCH_SIZE = 128  # for training batch size
+BATCH_SIZE = 1  # for training batch size
 timesteps = 16    # how many steps for a noisy image into clear
 time_bar = 1 - np.linspace(0, 1.0, timesteps + 1) # linspace for timesteps
-plt.plot(time_bar, label='Noise')
-plt.plot(1 - time_bar, label='Clarity')
-plt.legend()
-def cvtImg(img):
-    img = img - img.min()
-    img = (img / img.max())
-    return img.astype(np.float32)
+epochs = 1
 
-def show_examples(x):
-    plt.figure(figsize=(10, 10))
-    for i in range(25):
-        plt.subplot(5, 5, i+1)
-        img = cvtImg(x[i])
-        plt.imshow(img)
-        plt.axis('off')
-
-show_examples(X_train)
-def forward_noise(x, t):
-    a = time_bar[t]      # base on t
-    b = time_bar[t + 1]  # image for t + 1
+# main  
+t = func.generate_ts(25, timesteps=timesteps)             # random for training data
+a, b = func.forward_noise(X_train[:25], t, time_bar=time_bar)
     
-    noise = np.random.normal(size=x.shape)  # noise mask
-    a = a.reshape((-1, 1, 1, 1))
-    b = b.reshape((-1, 1, 1, 1))
-    img_a = x * (1 - a) + noise * a
-    img_b = x * (1 - b) + noise * b
-    return img_a, img_b
-    
-def generate_ts(num):
-    return np.random.randint(0, timesteps, size=num)
-
-# t = np.full((25,), timesteps - 1) # if you want see clarity
-# t = np.full((25,), 0)             # if you want see noisy
-t = generate_ts(25)             # random for training data
-a, b = forward_noise(X_train[:25], t)
-show_examples(a)
-def block(x_img, x_ts):
-    x_parameter = layers.Conv2D(128, kernel_size=3, padding='same')(x_img)
-    x_parameter = layers.Activation('relu')(x_parameter)
-
-    time_parameter = layers.Dense(128)(x_ts)
-    time_parameter = layers.Activation('relu')(time_parameter)
-    time_parameter = layers.Reshape((1, 1, 128))(time_parameter)
-    x_parameter = x_parameter * time_parameter
-    
-    # -----
-    x_out = layers.Conv2D(128, kernel_size=3, padding='same')(x_img)
-    x_out = x_out + x_parameter
-    x_out = layers.LayerNormalization()(x_out)
-    x_out = layers.Activation('relu')(x_out)
-    
-    return x_out
-def make_model():
-    x = x_input = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3), name='x_input')
-    
-    x_ts = x_ts_input = layers.Input(shape=(1,), name='x_ts_input')
-    x_ts = layers.Dense(192)(x_ts)
-    x_ts = layers.LayerNormalization()(x_ts)
-    x_ts = layers.Activation('relu')(x_ts)
-    
-    # ----- left ( down ) -----
-    x = x32 = block(x, x_ts)
-    x = layers.MaxPool2D(2)(x)
-    
-    x = x16 = block(x, x_ts)
-    x = layers.MaxPool2D(2)(x)
-    
-    x = x8 = block(x, x_ts)
-    x = layers.MaxPool2D(2)(x)
-    
-    x = x4 = block(x, x_ts)
-    
-    # ----- MLP -----
-    x = layers.Flatten()(x)
-    x = layers.Concatenate()([x, x_ts])
-    x = layers.Dense(128)(x)
-    x = layers.LayerNormalization()(x)
-    x = layers.Activation('relu')(x)
-
-    x = layers.Dense(4 * 4 * 32)(x)
-    x = layers.LayerNormalization()(x)
-    x = layers.Activation('relu')(x)
-    x = layers.Reshape((4, 4, 32))(x)
-    
-    # ----- right ( up ) -----
-    x = layers.Concatenate()([x, x4])
-    x = block(x, x_ts)
-    x = layers.UpSampling2D(2)(x)
-    
-    x = layers.Concatenate()([x, x8])
-    x = block(x, x_ts)
-    x = layers.UpSampling2D(2)(x)
-    
-    x = layers.Concatenate()([x, x16])
-    x = block(x, x_ts)
-    x = layers.UpSampling2D(2)(x)
-    
-    x = layers.Concatenate()([x, x32])
-    x = block(x, x_ts)
-    
-    # ----- output -----
-    x = layers.Conv2D(3, kernel_size=1, padding='same')(x)
-    model = tf.keras.models.Model([x_input, x_ts_input], x)
-    return model
-    
-
-model = make_model()
-# model.summary()
+model = func.make_model(IMG_SIZE=IMG_SIZE)
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0008)
 loss_func = tf.keras.losses.MeanAbsoluteError()
 model.compile(loss=loss_func, optimizer=optimizer)
-def predict(x_idx=None):
-    x = np.random.normal(size=(32, IMG_SIZE, IMG_SIZE, 3))
+func.predict(IMG_SIZE=IMG_SIZE, timesteps=timesteps, model=model)
+func.predict_step(IMG_SIZE=IMG_SIZE, timesteps=timesteps, model=model)
 
-    for i in trange(timesteps):
-        t = i
-        x = model.predict([x, np.full((32), t)], verbose=0)
-    show_examples(x)
-
-predict()
-def predict_step():
-    xs = []
-    x = np.random.normal(size=(8, IMG_SIZE, IMG_SIZE, 3))
-
-    for i in trange(timesteps):
-        t = i
-        x = model.predict([x, np.full((8),  t)], verbose=0)
-        if i % 2 == 0:
-            xs.append(x[0])
-
-    plt.figure(figsize=(20, 2))
-    for i in range(len(xs)):
-        plt.subplot(1, len(xs), i+1)
-        plt.imshow(cvtImg(xs[i]))
-        plt.title(f'{i}')
-        plt.axis('off')
-
-predict_step()
-def train_one(x_img):
-    x_ts = generate_ts(len(x_img))
-    x_a, x_b = forward_noise(x_img, x_ts)
-    loss = model.train_on_batch([x_a, x_ts], x_b)
-    return loss
-def train(R=1):
-    bar = trange(R)
-    total = 100
-    for i in bar:
-        for j in range(total):
-            x_img = X_train[np.random.randint(len(X_train), size=BATCH_SIZE)]
-            loss = train_one(x_img)
-            pg = (j / total) * 100
-            if j % 5 == 0:
-                bar.set_description(f'loss: {loss:.5f}, p: {pg:.2f}%')
- 
-for _ in range(5):
-    train()
+for _ in range(epochs):
+    func.train(X_train=X_train, BATCH_SIZE=BATCH_SIZE, R=1)
     # reduce learning rate for next training
     model.optimizer.learning_rate = max(0.000001, model.optimizer.learning_rate * 0.9)
 
     # show result 
-    predict()
-    predict_step()
+    func.predict(IMG_SIZE=IMG_SIZE, timesteps=timesteps, model=model)
+    func.predict_step(IMG_SIZE=IMG_SIZE, timesteps=timesteps, model=model)
     plt.show()
- 
- 
+
+
+    
--- diffusion_model/reference_main.py
+++ diffusion_model/reference_main.py
@@ -1,3 +1,8 @@
+
+"""
+For more detail:
+https://tree.rocks/make-diffusion-model-from-scratch-easy-way-to-implement-quick-diffusion-model-e60d18fd0f2e
+"""
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
@@ -5,6 +10,22 @@ from keras import layers
 import numpy as np
 from tqdm.auto import trange, tqdm
 import matplotlib.pyplot as plt
+
+for gpu in tf.config.list_physical_devices('GPU'):
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+# load dataset
+(X_train, y_train), (X_test, y_test) = tf.keras.datasets.cifar10.load_data()
+X_train = X_train[y_train.squeeze() == 1]
+X_train = (X_train / 127.5) - 1.0
+IMG_SIZE = 32     # input image size, CIFAR-10 is 32x32
+
+BATCH_SIZE = 128  # for training batch size
+timesteps = 16    # how many steps for a noisy image into clear
+time_bar = 1 - np.linspace(0, 1.0, timesteps + 1) # linspace for timesteps
+plt.plot(time_bar, label='Noise')
+plt.plot(1 - time_bar, label='Clarity')
+plt.legend()
 
 # normalization image
 def cvtImg(img):
@@ -22,7 +43,7 @@ def show_examples(x):
         plt.axis('off')
 
 # integrate noise
-def forward_noise(x, t, time_bar):
+def forward_noise(x, t):
     a = time_bar[t]      # base on t
     b = time_bar[t + 1]  # image for t + 1
     
@@ -34,9 +55,16 @@ def forward_noise(x, t, time_bar):
     return img_a, img_b
     
 # generate random vector
-def generate_ts(num, timesteps):
+def generate_ts(num):
     return np.random.randint(0, timesteps, size=num)
 
+
+# show_examples(X_train)
+# t = np.full((25,), timesteps - 1) # if you want see clarity
+# t = np.full((25,), 0)             # if you want see noisy
+t = generate_ts(25)             # random for training data
+a, b = forward_noise(X_train[:25], t)
+# show_examples(a)
 
 def block(x_img, x_ts):
     x_parameter = layers.Conv2D(128, kernel_size=3, padding='same')(x_img)
@@ -54,8 +82,7 @@ def block(x_img, x_ts):
     x_out = layers.Activation('relu')(x_out)
     
     return x_out
-
-def make_model(IMG_SIZE):
+def make_model():
     x = x_input = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3), name='x_input')
     
     x_ts = x_ts_input = layers.Input(shape=(1,), name='x_ts_input')
@@ -107,38 +134,46 @@ def make_model(IMG_SIZE):
     x = layers.Conv2D(3, kernel_size=1, padding='same')(x)
     model = tf.keras.models.Model([x_input, x_ts_input], x)
     return model
-
-
-def predict(IMG_SIZE, timesteps, model, x_idx=None):
+    
+# main
+model = make_model()
+# model.summary()
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0008)
+loss_func = tf.keras.losses.MeanAbsoluteError()
+model.compile(loss=loss_func, optimizer=optimizer)
+def predict(x_idx=None):
     x = np.random.normal(size=(32, IMG_SIZE, IMG_SIZE, 3))
+
     for i in trange(timesteps):
         t = i
         x = model.predict([x, np.full((32), t)], verbose=0)
-    #show_examples(x)
-    
-def predict_step(IMG_SIZE, timesteps, model):
+    show_examples(x)
+
+predict()
+def predict_step():
     xs = []
     x = np.random.normal(size=(8, IMG_SIZE, IMG_SIZE, 3))
+
     for i in trange(timesteps):
         t = i
         x = model.predict([x, np.full((8),  t)], verbose=0)
         if i % 2 == 0:
             xs.append(x[0])
 
-    '''plt.figure(figsize=(20, 2))
+    plt.figure(figsize=(20, 2))
     for i in range(len(xs)):
         plt.subplot(1, len(xs), i+1)
         plt.imshow(cvtImg(xs[i]))
         plt.title(f'{i}')
-        plt.axis('off')'''
+        plt.axis('off')
 
-def train_one(x_img, model):
+predict_step()
+def train_one(x_img):
     x_ts = generate_ts(len(x_img))
     x_a, x_b = forward_noise(x_img, x_ts)
     loss = model.train_on_batch([x_a, x_ts], x_b)
     return loss
-
-def train(X_train, BATCH_SIZE, R):
+def train(R=1):
     bar = trange(R)
     total = 100
     for i in bar:
@@ -148,4 +183,15 @@ def train(X_train, BATCH_SIZE, R):
             pg = (j / total) * 100
             if j % 5 == 0:
                 bar.set_description(f'loss: {loss:.5f}, p: {pg:.2f}%')
+ 
+for _ in range(5):
+    train()
+    # reduce learning rate for next training
+    model.optimizer.learning_rate = max(0.000001, model.optimizer.learning_rate * 0.9)
+
+    # show result 
+    predict()
+    predict_step()
+    plt.show()
+ 
  
